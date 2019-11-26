00d2e582b8d2118c96536f9950d7fc6879639315
JulesDisch <53347157+JulesDisch@users.noreply.github.com>
2019-11-25 19:35:00 -0500

    bringing it all together

--- models/author.js
+++ models/author.js
@@ -1,24 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-<<<<<<< HEAD
-    var Author = sequelize.define("Author", {
-      // Giving the Author model a name of type STRING
-      name: DataTypes.STRING
-    });
-  
-    Author.associate = function(models) {
-      // Associating Author with Posts
-      // When an Author is deleted, also delete any associated Posts
-      Author.hasMany(models.Post, {
-        onDelete: "cascade"
-      });
-    };
-  
-    return Author;
-  };
-=======
   var Author = sequelize.define("Author", {
     // Giving the Author model a name of type STRING
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
   });
 
   Author.associate = function(models) {
@@ -31,4 +14,3 @@ module.exports = function(sequelize, DataTypes) {
 
   return Author;
 };
->>>>>>> e1ac4bcd5b3a37366f5ebb798ac311bb91dc3efd
